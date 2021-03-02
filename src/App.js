4a9a5a43f61471189d70a4d7a4a6e4b888b23aaf
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-03-02 16:20:36 -0300

    [props] [s] : add page pagination props

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import TableContainer from './TableContainer';
 
 function App({ dataset, schema }) {
   const [filter, setFilter] = useState({});
+  const [ total, setTotal ] = useState(0)
 
   return (
     <div className='max-w-2xl mx-auto mt-20 '>
       <p data-testid="hidden-test"></p>
-      <Filter dataset={dataset} schema={schema} filter={filter} setFilter={setFilter} />
-      <TableContainer dataset={dataset} schema={schema} filter={filter} />
+      <Filter dataset={dataset} schema={schema} filter={filter} setFilter={setFilter} total={total} setTotal={setTotal} />
+      <TableContainer dataset={dataset} schema={schema} filter={filter} total={total} />
     </div>
   );
 }
