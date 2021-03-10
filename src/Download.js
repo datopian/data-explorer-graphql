dc15a7a7fc2242f9cd2f7258936be84d647d2396
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-03-10 17:23:10 -0300

    [download] [s] : add download method

--- src/Download.js
+++ src/Download.js
@@ -1,20 +1,41 @@
 import React, { useState } from 'react'
 
-export default function Download() {
-const [selected, setSelected] = useState('')
+export default function Download({query}) {
+const [selected, setSelected] = useState('json')
 const options = ['json', 'csv', 'xlsx', 'tsv', 'ods']
+const {downloading, setDownloading } = useState('')
 
 const handleChange =(event)=> {
   setSelected(event.target.value)
 }
 
+const downloadData = ()=>{
+  setDownloading('Preparing Download')
+  fetch(`http://localhost:8080/v1/download?format=${selected}`,{
+    method: 'POST',
+    headers: {'Content-Type' : 'application/json', 'Content-Disposition' : 'attachment', filename: `report.${selected}`}, 
+    body: JSON.stringify({'query':query} )
+}).then(response => response.blob())
+.then(blob => {
+            setDownloading('Downloading file')
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = `report.${selected}`;
+            document.body.appendChild(a); 
+            a.click();    
+            a.remove(); 
+            setDownloading('Done.')        
+        }).catch(() => setDownloading('Error on download file'))
+
+}
 
   return (
     <div className='p-5 flex justify-around'>
       Select format to Download: <select onChange={handleChange}>
         {options.map(item => (<option value={item}>{item}</option>))}
       </select>
-    <button className='bg-blue-600 p-1 text-white  rounded-md'> Download </button>
+    <button onClick={()=> downloadData()} className='bg-blue-600 p-1 text-white  rounded-md'> Download </button>
     </div>
   )
 }
