fabbc202e5c2ef66f7b81f91a4ad24cd4408ed2c
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-03-11 16:11:16 -0300

    [download] [s] : update download query to download file

--- src/Download.js
+++ src/Download.js
@@ -1,41 +1,57 @@
 import React, { useState } from 'react'
+import axios from 'axios'
+import fileDownload from 'js-file-download'
 
-export default function Download({query}) {
-const [selected, setSelected] = useState('json')
-const options = ['json', 'csv', 'xlsx', 'tsv', 'ods']
-const {downloading, setDownloading } = useState('')
+export default function Download({ query }) {
+  const [selected, setSelected] = useState('json')
+  const options = ['json', 'csv', 'xlsx', 'tsv', 'ods']
+  const [downloading, setDownloading] = useState('')
 
-const handleChange =(event)=> {
-  setSelected(event.target.value)
-}
+  const handleChange = (event) => {
+    setSelected(event.target.value)
+  }
 
-const downloadData = ()=>{
-  setDownloading('Preparing Download')
-  fetch(`http://localhost:8080/v1/download?format=${selected}`,{
-    method: 'POST',
-    headers: {'Content-Type' : 'application/json', 'Content-Disposition' : 'attachment', filename: `report.${selected}`}, 
-    body: JSON.stringify({'query':query} )
-}).then(response => response.blob())
-.then(blob => {
-            setDownloading('Downloading file')
-            const url = window.URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = `report.${selected}`;
-            document.body.appendChild(a); 
-            a.click();    
-            a.remove(); 
-            setDownloading('Done.')        
-        }).catch(() => setDownloading('Error on download file'))
-
-}
+  const downloadData = () => {
+    setDownloading('Preparing Download')
+    axios(
+      `https://data-api.energidataservice.dk/v1/download?format=${selected}`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        responseType: 'blob',
+        data: {
+          query: `query Dataset {
+                    ${query}
+                    }
+                  `,
+        },
+      }
+    )
+      .then((response) => {
+        setDownloading('Downloading file')
+        fileDownload(response.data, `data.${selected}`)
+        setDownloading('Done.')
+        setTimeout(() => setDownloading(''), 5000)
+      })
+      .catch((error) => setDownloading(error.message))
+  }
 
   return (
-    <div className='p-5 flex justify-around'>
-      Select format to Download: <select onChange={handleChange}>
-        {options.map(item => (<option value={item}>{item}</option>))}
+    <div className="p-5 flex justify-around">
+      Select format to Download:{' '}
+      <select onChange={handleChange}>
+        {options.map((item) => (
+          <option value={item}>{item}</option>
+        ))}
       </select>
-    <button onClick={()=> downloadData()} className='bg-blue-600 p-1 text-white  rounded-md'> Download </button>
+      <button
+        onClick={() => downloadData()}
+        className="bg-blue-600 p-1 text-white  rounded-md"
+      >
+        {' '}
+        Download{' '}
+      </button>
+      <p>{downloading}</p>
     </div>
   )
 }
