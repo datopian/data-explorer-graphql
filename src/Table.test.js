fab62306936cee2ea6a0d27c150ea7e721752831
Rising Odegua <risingodegua@gmail.com>
2021-03-12 14:31:51 +0100

    [Test: Table][m]: Add Table component test

---
+++ src/Table.test.js
@@ -0,0 +1,19 @@
+import { render, act, screen } from "@testing-library/react";
+import Table from "./Table";
+import React from "react";
+import { args } from "./args";
+import results from "./fixtures/results.json";
+
+test("renders Table without failing", async () => {
+  act(() => {
+    render(
+      <Table
+        data={results["transmissionlines"]}
+        schema={args.schema}
+        dataset={args.dataset}
+        total={ results["transmissionlines"].length}
+      ></Table>
+    );
+  });
+  expect(screen.getByTestId(/reactTable/i)).toBeInTheDocument();
+});
