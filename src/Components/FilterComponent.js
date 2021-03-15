eb8e5485eef8b8d418f3697f3cbba16633aa91ba
steveoni <steohenoni2@gmail.com>
2021-03-15 10:07:23 +0000

    [FilterComponent][l]: move DateTime filter subcomponent into a new file containing all filter component

---
+++ src/Components/FilterComponent.js
@@ -0,0 +1,65 @@
+import React, { useState, useRef } from 'react'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+export function DateTime({columnName, setInputStates, index, fields}) {
+  const [startDate1, setStartDate1] = useState();
+  const [startDate2, setStartDate2] = useState();
+
+  const handleDate = function(columnName,date, type) {
+    if (date) {
+      const dDate = date.toISOString().slice(0,10)
+      const hour = date.getHours()
+      const minute = date.getMinutes().toString()
+      let timeString = null;
+
+      console.log(fields, columnName)
+      const examples = fields.filter(val => val.name === columnName)[0]["example"]
+      const isISO= examples.includes('Z')
+    
+      if (isISO) {
+        timeString = `${dDate} ${hour}:${minute}${ minute.length > 1? 'Z' : '0Z'}`
+      } else {
+        timeString = `${dDate} ${hour}:${minute}`
+      }
+      
+      if (type === "type1") {
+
+        setStartDate1(date)
+        setInputStates((prevState) =>{
+          const newdata = prevState.slice();
+          newdata[index]['logicValue'][0] = "_gte"
+          newdata[index]["inputValue"][0] = timeString;
+          return newdata;
+      });
+      } else {
+        setStartDate2(date)
+        setInputStates((prevState) =>{
+          const newdata = prevState.slice();
+          newdata[index]['logicValue'][1] = "_lt"
+          newdata[index]["inputValue"][1] = timeString;
+          return newdata;
+        });
+      }
+    } else {
+
+      if (type === "type1") {
+
+        setStartDate1(date)
+      } else {
+        setStartDate2(date)
+      }
+
+    }
+    
+  }
+
+  return (
+    <>
+      <DatePicker selected={startDate1} onChange={date => handleDate(columnName,date, "type1")} showTimeSelect dateFormat="yyyy-MM-dd HH:mm" timeFormat="HH:mm"/>
+      <span className="mr-2">--></span>
+      <DatePicker selected={startDate2} onChange={date => handleDate(columnName,date, "type2")} showTimeSelect dateFormat="yyyy-MM-dd HH:mm" timeFormat="HH:mm"/>
+
+    </>
+  );
+}
