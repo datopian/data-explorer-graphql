0d89a3ee2feb03de12b9270638056eb20ce30b41
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-03-16 22:27:39 -0300

    [filter] [m] : add filter to count filtered data

--- src/Filter.js
+++ src/Filter.js
@@ -1,80 +1,100 @@
 import React, { useState, useRef } from 'react'
-import { useQuery, gql } from '@apollo/client';
-import { SelectFilter, OrderBy } from './Components/FilterComponents';
-const Query = require('graphql-query-builder');
+import { useQuery, gql } from '@apollo/client'
+import { SelectFilter, OrderBy } from './Components/FilterComponents'
+const Query = require('graphql-query-builder')
 
+function Filter({
+  dataset,
+  schema,
+  filter,
+  setFilter,
+  total,
+  setTotal,
+  setOffset,
+  setPage,
+}) {
 
-function Filter({ dataset, schema, filter, setFilter, total, setTotal, setOffset, setPage }) {
+  const newFilter = {}
 
+  if(filter.where) Object.assign(newFilter, { where: filter.where})
+  
   const getTotalRows = new Query(`${dataset}_aggregate`)
-    .find(new Query('aggregate').find('count'));
+  .filter(newFilter)
+  .find(
+    new Query('aggregate')
+    .find('count')
+    )
+  
 
   // getTotalRows.filter({where: {ConnectedArea: {_eq: 'DK1'}}});
   const QUERY = gql`
     query Dataset {
       ${getTotalRows}
     }
-  `;
+  `
 
-
-  const [inputStates, setInputStates] = useState([{columnName:[''], logicValue: [], inputValue: []}])
+  const [inputStates, setInputStates] = useState([
+    { columnName: [''], logicValue: [], inputValue: [] },
+  ])
   const orderColumnRef = useRef()
   const orderByRef = useRef()
 
-  const { loading, error, data } = useQuery(QUERY);
+  const { loading, error, data } = useQuery(QUERY)
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-    if (data) {
-      setTotal(data[`${dataset}_aggregate`].aggregate.count);
+  if (loading) return <p>Loading...</p>
+  if (error) {
+    console.log(error)
+    return <p>Error :(</p>
     }
+
+  if (data) {
+    setTotal(data[`${dataset}_aggregate`].aggregate.count)
+  }
 
   const logics = {
-    '==' : '_eq',
-    '<' : '_lt',
-    '>' : '_gt',
-    '<=' : '_lte',
-    '!=' : '_neq',
-    '>=' : '_gte',
+    '==': '_eq',
+    '<': '_lt',
+    '>': '_gt',
+    '<=': '_lte',
+    '!=': '_neq',
+    '>=': '_gte',
   }
-  const filterTable = function() {
-    const whereVariables = {};
+  const filterTable = function () {
+    const whereVariables = {}
     const filterVariables = {}
     inputStates.forEach((value, index) => {
-      
-      if (value.inputValue[0]) { // ensure no empty input value
+      if (value.inputValue[0]) {
+        // ensure no empty input value
         whereVariables[value.columnName[0]] = {}
-        value.logicValue.forEach((val, index)=> {
-          const logic = value.logicValue[index];
-          const input = value.inputValue[index];
-          whereVariables[value.columnName][logic] = input;
+        value.logicValue.forEach((val, index) => {
+          const logic = value.logicValue[index]
+          const input = value.inputValue[index]
+          whereVariables[value.columnName][logic] = input
         })
       }
-      
-    });
-    filterVariables['where'] = whereVariables;
+    })
+    filterVariables['where'] = whereVariables
 
-    const whereKeys = Object.keys(whereVariables);
+    const whereKeys = Object.keys(whereVariables)
     const length = whereKeys.length
-    const isFilled = length === 1 ? whereKeys[0].length : true;
+    const isFilled = length === 1 ? whereKeys[0].length : true
 
-    if( isFilled) {
-      filterVariables['where'] = whereVariables;
+    if (isFilled) {
+      filterVariables['where'] = whereVariables
     }
-    
-    const orderColumn = orderColumnRef.current.value;
-    const orderBy = orderByRef.current.value;
-    filterVariables['order_by'] = {[orderColumn]: orderBy}
+
+    const orderColumn = orderColumnRef.current.value
+    const orderBy = orderByRef.current.value
+    filterVariables['order_by'] = { [orderColumn]: orderBy }
     filterVariables['limit'] = 100
 
-    setFilter(filterVariables);
+    setFilter(filterVariables)
   }
 
-  const resetFilter = function() {
+  const resetFilter = function () {
     // reset the inputstates to one if multiple exist
-    if (inputStates.length > 1 ) {
-      setInputStates([{columnName:[''], logicValue: [], inputValue: []}]);
+    if (inputStates.length > 1) {
+      setInputStates([{ columnName: [''], logicValue: [], inputValue: [] }])
     }
     setOffset(0)
     setPage(0)
@@ -83,28 +103,49 @@ function Filter({ dataset, schema, filter, setFilter, total, setTotal, setOffset
 
   return (
     <>
-    <div data-testid='agg'>
-      Total rows: {total}
-    </div>
-    <form>
-      <div className='mb-2 border pl-2' data-testid='all-fields'>
-        {inputStates.map((value, index) => {
-
-          return <SelectFilter setInputStates={setInputStates} fields={schema.fields} logics={logics}
-                             inputState={value} inputStates={inputStates} index={index} key={index}/>
-        })}
+      <div data-testid="agg">Total rows: {total}</div>
+      <form>
+        <div className="mb-2 border pl-2" data-testid="all-fields">
+          {inputStates.map((value, index) => {
+            return (
+              <SelectFilter
+                setInputStates={setInputStates}
+                fields={schema.fields}
+                logics={logics}
+                inputState={value}
+                inputStates={inputStates}
+                index={index}
+                key={index}
+              />
+            )
+          })}
+        </div>
+      </form>
+      <OrderBy
+        orderColumnRef={orderColumnRef}
+        orderByRef={orderByRef}
+        fields={schema.fields}
+      />
+      <div>
+        <button
+          onClick={() => {
+            filterTable()
+          }}
+          className="bg-blue-600 p-2 text-white  rounded-md mr-4"
+        >
+          Submit
+        </button>
+        <button
+          onClick={() => {
+            resetFilter()
+          }}
+          className="bg-green-600 p-2 text-white  rounded-md"
+        >
+          Reset
+        </button>
       </div>
-
-    </form>
-    <OrderBy orderColumnRef={orderColumnRef} orderByRef={orderByRef} fields={schema.fields}/>
-    <div>
-        <button onClick={()=> { filterTable()}} className='bg-blue-600 p-2 text-white  rounded-md mr-4'>Submit</button>
-        <button onClick={()=> { resetFilter()}} className='bg-green-600 p-2 text-white  rounded-md'>Reset</button>
-    </div>
-    
     </>
-
-  );
+  )
 }
 
-export default Filter;
+export default Filter
