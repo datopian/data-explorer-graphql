8eaee68ec78231288bd253d85deb117980ef8ec3
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-03-19 18:40:24 -0300

    [buttom] [l] : add copy buttom function and style

--- src/Components/CopyButtom/CopyButtom.js
+++ src/Components/CopyButtom/CopyButtom.js
@@ -2,5 +2,23 @@ import React from 'react'
 
 export default function CopyButton({ query }) {
   console.log(query)
-  return <button>Copy Query</button>
+
+  const copyQuery = () => {
+    const area = document.createElement('textarea')
+    area.value = query
+    document.body.appendChild(area)
+    area.focus()
+    area.select()
+    document.execCommand('copy')
+    document.body.removeChild(area)
+  }
+
+  return (
+    <button
+      className="bg-blue-600 bg-opacity-75 p-2 text-white  rounded-md mr-4"
+      onClick={() => copyQuery()}
+    >
+      Copy Query
+    </button>
+  )
 }
