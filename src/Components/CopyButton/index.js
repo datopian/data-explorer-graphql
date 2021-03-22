d60a9a2c24627ed10d7136b71f5adfb5141273c3
anuveyatsu <anuar.ustayev@gmail.com>
2021-03-22 17:50:01 +0600

    [copy to clipboard][s]: use common react lib for this instead of custom code.

--- src/Components/CopyButton/index.js
+++ src/Components/CopyButton/index.js
@@ -1,2 +1,10 @@
-import CopyButton from './CopyButton'
-export default CopyButton
+import React from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+
+export default function CopyButton({ query }) {
+  return (
+    <CopyToClipboard text={query}>
+      <button>Copy query</button>
+    </CopyToClipboard>
+  )
+}
