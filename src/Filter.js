461d8130ab13c0b9d65fec8dfdc63596bef7d4fb
steveoni <steohenoni2@gmail.com>
2021-03-29 21:04:41 +0000

    [Filter.js][m]: delete all graphql query in filter.js and moved them to a new component, to prevent filter component reloading

--- src/Filter.js
+++ src/Filter.js
@@ -1,7 +1,5 @@
 import React, { useState, useRef } from 'react'
-import { useQuery, gql } from '@apollo/client'
-import { SelectFilter, OrderBy } from './Components/FilterComponents'
-import spinner from './spinner.svg'
+import { SelectFilter, OrderBy, Totalrows } from './Components/FilterComponents'
 import CopyButton from './Components/CopyButton'
 const Query = require('graphql-query-builder')
 
@@ -20,37 +18,13 @@ function Filter({
 
   if (filter && filter.where) Object.assign(newFilter, { where: filter.where })
 
-  const getTotalRows = new Query(`${dataset}_aggregate`)
-    .filter(newFilter)
-    .find(new Query('aggregate').find('count'))
-
-  // getTotalRows.filter({where: {ConnectedArea: {_eq: 'DK1'}}});
-  const QUERY = gql`
-    query Dataset {
-      ${getTotalRows}
-    }
-  `
 
   const [inputStates, setInputStates] = useState([
     { columnName: [''], logicValue: [], inputValue: [] },
   ])
-
   const orderColumnRef = useRef()
   const orderByRef = useRef()
   const [addRules, setAddRules] = useState(false)
-
-  const { loading, error, data } = useQuery(QUERY)
-
-  if (loading)
-    return <img src={spinner} className="spinner" alt="Loading..." />
-  if (error) {
-    console.log(error)
-    return <p>Error :(</p>
-  }
-
-  if (data) {
-    setTotal(data[`${dataset}_aggregate`].aggregate.count)
-  }
 
   const logics = {
     '==': '_eq',
@@ -90,13 +64,13 @@ function Filter({
     const orderBy = orderByRef.current.value
     filterVariables['order_by'] = { [orderColumn]: orderBy }
     filterVariables['limit'] = 100
-
+    // console.log(filterVariables);
     setFilter(filterVariables)
   }
 
   const resetFilter = function () {
     // reset the inputstates to one if multiple exist
-    if (inputStates[0].inputValue.length || inputStates.length > 1) {
+    if (inputStates[0].inputValue.length  || inputStates.length > 1) {
       setInputStates([{ columnName: [''], logicValue: [], inputValue: [] }])
       setOffset(0)
       setPage(0)
@@ -119,9 +93,12 @@ function Filter({
     <div className="dq-main-container">
       <div className="dq-heading">
         <div className="dq-heading-main"></div>
-        <div data-testid="agg" className="dq-heading-total-rows">
-          Total rows: {total && total.toLocaleString()}
-        </div>
+        <Totalrows 
+          newFilter={newFilter} 
+          dataset={dataset} 
+          setTotal={setTotal}
+          total={total}
+        />
       </div>
       <form>
         <div data-testid="all-fields">
