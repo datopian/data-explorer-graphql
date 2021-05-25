2125e287903de2b002ee4cbc50a0570c53a2a63f
Tanvir Chahal <tnvr009@gmail.com>
2021-05-24 20:19:50 -0400

    wip test filters and tables

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -25,4 +25,24 @@ describe("Renders App", () => {
         })
         cy.get('.react-date-picker__inputGroup').children('[type="date"]').should('not.be.visible')
     })
+
+    it("Check if filters work", () => {
+        cy.visit("/")
+        cy.get('.dq-rule-add').click()
+
+        const columnName = cy.get('[name="columnName"]').invoke('text')
+        cy.log(columnName)
+
+        cy.get('[data-testid=field-value]').clear()
+        cy.get('[data-testid=field-value]').type('DK2')
+        cy.get('.submit-button').click()
+
+        cy.get(".rt-tr-group .rt-td:nth-child(3)").each(($e1, index, $list) => {
+            const text = $e1.text();
+            if(!(text.includes("DK2") || text.includes(""))) {
+                throw new Error("Filters not working correctly")
+            }
+        })
+    })
+
 })
