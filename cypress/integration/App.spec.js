fec2bebb975f78d47c67c56579c0884506eeafd6
Tanvir Chahal <tnvr009@gmail.com>
2021-05-25 21:19:20 -0400

    Code cleanup

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -38,7 +38,7 @@ describe("Renders App", () => {
         cy.get('.submit-button').click()
 
         cy.get(".rt-tr-group .rt-td:nth-child(3)").each(($e1, index, $list) => {
-            const text = $e1.text();
+            const text = $e1.text()
             if(!(text.includes("DK2") || text.includes(""))) {
                 throw new Error("Filters not working correctly")
             }
@@ -53,23 +53,23 @@ describe("Renders App", () => {
         cy.get('.react-date-picker--open > .react-date-picker__wrapper > .react-date-picker__inputGroup > .react-date-picker__inputGroup__month').type('05');
         cy.get('.react-date-picker--open > .react-date-picker__wrapper > .react-date-picker__inputGroup > .react-date-picker__inputGroup__day').type('11');
 
-        cy.get('.react-date-picker--closed > .react-date-picker__wrapper > .react-date-picker__inputGroup > .react-date-picker__inputGroup__year').click();
-        cy.get('.react-date-picker--open > .react-date-picker__wrapper > .react-date-picker__inputGroup > .react-date-picker__inputGroup__year').type('2021');
-        cy.get('.react-date-picker--open > .react-date-picker__wrapper > .react-date-picker__inputGroup > .react-date-picker__inputGroup__month').type('05');
-        cy.get('.react-date-picker--open > .react-date-picker__wrapper > .react-date-picker__inputGroup > .react-date-picker__inputGroup__day').type('12');
+        cy.get('.react-date-picker--closed > .react-date-picker__wrapper > .react-date-picker__inputGroup > .react-date-picker__inputGroup__year').click()
+        cy.get('.react-date-picker--open > .react-date-picker__wrapper > .react-date-picker__inputGroup > .react-date-picker__inputGroup__year').type('2021')
+        cy.get('.react-date-picker--open > .react-date-picker__wrapper > .react-date-picker__inputGroup > .react-date-picker__inputGroup__month').type('05')
+        cy.get('.react-date-picker--open > .react-date-picker__wrapper > .react-date-picker__inputGroup > .react-date-picker__inputGroup__day').type('12')
         
-        cy.get('.data-download-default > select').select('json');
+        cy.get('.data-download-default > select').select('json')
         cy.get('.submit-button').click()
 
-        cy.get("#download-data").should("be.visible").click();
-        cy.wait(3000);
+        cy.get("#download-data").should("be.visible").click()
+        cy.wait(3000)
 
         cy.get('#root').attribute('data-dataset').then((res) => {
             const dataset = res
             const today = new Date()
-            const dd = String(today.getDate());
-            const mm = String(today.getMonth() + 1);
-            const yyyy = today.getFullYear().toString();
+            const dd = String(today.getDate())
+            const mm = String(today.getMonth() + 1)
+            const yyyy = today.getFullYear().toString()
 
             cy.readFile(`cypress/downloads/${dataset}_${mm}_${dd}_${yyyy}.json`)
         })
